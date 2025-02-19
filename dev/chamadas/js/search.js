03399ed2af4878cff68c1bc20c7f9e536e6206f9
GABIBACK13 <gabrielback2006oficial@gmail.com>
2025-02-19 00:53:02 -0300

    Adiciona página de pesquisa: cria estrutura HTML, estilos CSS e lógica JavaScript para a funcionalidade de busca

---
+++ dev/chamadas/js/search.js
@@ -0,0 +1,11 @@
+const chamadaList = document.querySelector('#chamadas-list');
+const homeList = document.querySelector('#home-list');
+const dataViewList = document.querySelector('#data-view-list');
+const searchList = document.querySelector('#search-list');
+
+document.addEventListener('DOMContentLoaded', () => {
+  chamadaList.classList.remove('selected');
+  homeList.classList.remove('selected');
+  dataViewList.classList.remove('selected');
+  searchList.classList.add('selected');
+});
